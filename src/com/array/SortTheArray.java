24490d18a4780acfea8b0b1735b7df876c012b81
kunal <kunaltech508@gmail.com>
2024-10-17 10:08:14 +0530

    eighth commit on master

--- src/com/array/SortTheArray.java
+++ src/com/array/SortTheArray.java
@@ -2,5 +2,25 @@ package com.array;
 
 public class SortTheArray {
     public static void main(String[] args) {
+        int[] array = {85, 69, 23, 14, 33};
+        int[] sortedArray = sortTheArray(array);
+        System.out.print("Sorted Array: ");
+        for( int i : sortedArray ){
+            System.out.print(i + " ");
+        }
+
+
+    }
+    static int[] sortTheArray( int[] arr ){
+        for (int i = 0; i < arr.length - 1; i++) {
+            for (int j = i + 1; j < arr.length; j++) {
+                 if( arr[i] > arr[j]){
+                     int temp = arr[i];
+                     arr[i] = arr[j];
+                     arr[j] = temp;
+                 }
+            }
+        }
+        return arr;
     }
 }

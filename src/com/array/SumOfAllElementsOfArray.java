f6ddcb5b26dd731141b515a1fdb8906a33f21434
kunal <kunaltech508@gmail.com>
2024-10-17 10:31:03 +0530

    ninth commit on master

--- src/com/array/SumOfAllElementsOfArray.java
+++ src/com/array/SumOfAllElementsOfArray.java
@@ -4,12 +4,28 @@ public class SumOfAllElementsOfArray {
     public static void main(String[] args) {
         int[] array = {45, 52, 96, 25, 10};
         int result = addElements(array);
+        System.out.print("sum of all elements in the array is : ");
         System.out.println(result);
     }
       public static int addElements(int[] nums){
+
+  /*
+        using for loop
           int sum = 0;
           for (int i = 0; i < nums.length; i++) {
                   sum += nums[i];
+          }
+          return sum;
+*/
+
+          
+//           using while loop
+
+          int i = 0;
+          int sum = 0;
+          while ( i < nums.length ){
+              sum += nums[i];
+              i++;
           }
           return sum;
       }
